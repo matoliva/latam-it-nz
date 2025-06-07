662620e5793129223c199e91f74508f34a190052
matoliva <olivamati@gmail.com>
2025-06-08 00:27:38 +1200

    fix: fix async script load

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -157,7 +157,7 @@ export default async function RootLayout({
           {children}
           <Footer lang={lang} translations={dictionary} />
         </ThemeProvider>
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="matiasoliva" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FD0" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+        <script async data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="matiasoliva" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FD0" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </html>
   );
