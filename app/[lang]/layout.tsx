9ccbb1a5138accf71daba7fde47009427e3eb75e
matoliva <olivamati@gmail.com>
2025-06-08 00:33:10 +1200

    fix: widget load

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -142,6 +142,7 @@ export default async function RootLayout({
       <head>
         <meta name="permissions-policy" content="payment=()" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <script async data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="matiasoliva" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FD0" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </head>
       <body 
         className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}
@@ -157,7 +158,6 @@ export default async function RootLayout({
           {children}
           <Footer lang={lang} translations={dictionary} />
         </ThemeProvider>
-        <script async data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="matiasoliva" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FD0" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </html>
   );
