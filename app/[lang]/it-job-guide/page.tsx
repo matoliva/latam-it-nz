863f9b1f7baf4db5eb553089324c194b33f8dc67
Matias Oliva <matias.oliva@thecollective.energy>
2025-06-16 14:17:53 +1200

    fix error

--- app/[lang]/it-job-guide/page.tsx
+++ app/[lang]/it-job-guide/page.tsx
@@ -1,8 +1,9 @@
 import Content from './content.mdx';
 import DownloadPdfLink from '@/components/DownloadPdfLink';
 
-export default function ItJobGuidePage({ params }: { params: { lang: string } }) {
-  const lang = params.lang || 'es';
+export default async function ItJobGuidePage({ params }: { params: Promise<any> }) {
+  const resolvedParams = await params as { lang: string };
+  const lang = resolvedParams.lang || 'es';
   return (
     <div className="max-w-3xl mx-auto px-4 pt-10 pb-10">
       <div className="flex justify-end mb-4">
