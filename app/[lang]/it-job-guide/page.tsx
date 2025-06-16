60141d2f2e0e62a148c1ddb0219586682edba863
Matias Oliva <matias.oliva@thecollective.energy>
2025-06-16 15:04:59 +1200

    fix layout

--- app/[lang]/it-job-guide/page.tsx
+++ app/[lang]/it-job-guide/page.tsx
@@ -4,9 +4,13 @@ import ClientDownloadPdfLink from '@/components/ClientDownloadPdfLink';
 export default async function ItJobGuidePage() {
   return (
     <div className="max-w-3xl mx-auto px-4 pt-10 pb-10">
-      <Content />
-      <div className="mt-10">
-        <ClientDownloadPdfLink />
+      <div className="flex flex-col md:flex-row justify-between items-start">
+        <div className="w-full">
+          <Content />
+        </div>
+        <div className="md:ml-4 mt-4 md:mt-0 flex-shrink-0">
+          <ClientDownloadPdfLink />
+        </div>
       </div>
     </div>
   );
