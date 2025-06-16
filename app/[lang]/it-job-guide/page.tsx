9a08228aa025896a57c787ff87aec9940493eb84
matoliva <olivamati@gmail.com>
2025-06-16 22:45:27 +1200

    fix btn alignment

--- app/[lang]/it-job-guide/page.tsx
+++ app/[lang]/it-job-guide/page.tsx
@@ -4,13 +4,11 @@ import ClientDownloadPdfLink from '@/components/ClientDownloadPdfLink';
 export default async function ItJobGuidePage() {
   return (
     <div className="max-w-3xl mx-auto px-4 pt-10 pb-10">
-      <div className="flex flex-col md:flex-row justify-between items-start">
-        <div className="w-full">
-          <Content />
-        </div>
-        <div className="md:ml-4 mt-4 md:mt-0 flex-shrink-0">
-          <ClientDownloadPdfLink />
-        </div>
+      <div className="flex justify-end mb-6">
+        <ClientDownloadPdfLink />
+      </div>
+      <div className="w-full">
+        <Content />
       </div>
     </div>
   );
